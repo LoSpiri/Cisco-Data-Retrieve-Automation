14b2fcd39dbf8caf8de1639ceb26db32702a76fa
LoSpiri <spiri16@protonmail.com>
2022-08-26 17:37:50 +0200

    First attempt

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
-
 from docx import Document
 from docx.shared import Pt
 from cisco import *
 from bookwriting import *
+import threading
 import pandas as pd
 import getpass
 
-def run():
-    # could return tuple
-    # username
-    username = input('Username: ')
-    # password  
-    password = getpass.getpass("Password: ")
-    # idf_number
-    distribution_hostname = input('Distribution switch hostname, with XXX replacing the idf number: ').strip()
-    prev, next = distribution_hostname.split('XXX')
-    if next[-11:] != '.amazon.com':
-        next = next + '.amazon.com'
 
-    ups_vlan = input("Enter UPS vlan (Retrievable running 'sh vlan brief' in any device) :")
+class myThread (threading.Thread):
+   def __init__(self, numberlock, errorlock):
+      threading.Thread.__init__(self)
+      self.numberlock = numberlock
+      self.errorlock = errorlock
+   def run(self):
+      print ("Starting IDF " + self.number)
+      create_book(self.numberlock, self.errorlock)
+      print ("Exiting IDF " + self.number)
 
-    device_models = []
-    while(True):
-        full_model = input("Insert all access points models, when finished input 'end' (Retrievable running 'sh cdp ne' in an acc-sw): ")
-        model = full_model.split('-')[0]
-        if model == 'end': break
-        device_models.append(model)
 
-    number = 0
-    errors = 0
 
-    while errors<3:
+def create_book(numberlock, errorlock):
+    while(error < 3):
+        numberlock.acquire()
         number += 1
+        numberlock.release()
         idf_number = str(number)
 
         dis_name = prev + idf_number + next
@@ -45,8 +37,9 @@ def run():
 
         net_connect_dis_sw = connect_to_cisco(username,password,dis_name)
         if net_connect_dis_sw == 1:
-            errors += 1
-            continue
+            errorlock.acquire()
+            error += 1
+            errorlock.release()
         print("Proceeding to write IDF book for: " + dis_name)
 
         # Retrieving switch set
@@ -101,6 +94,42 @@ def run():
         df_to_table_at_position(document,ups_df,'UPS')
 
         document.save('IDF' + idf_number + ' Book.docx')
+    
+    return 0
+
+
+
+
+def run():
+    # could return tuple
+    # username
+    global username, password, prev, next, device_models, ups_vlan, number, error
+    numberlock = threading.Lock()
+    errorlock = threading.Lock()
+    username = input('Username: ')
+    # password  
+    password = getpass.getpass("Password: ")
+    # idf_number
+    distribution_hostname = input('Distribution switch hostname, with XXX replacing the idf number: ').strip()
+    prev, next = distribution_hostname.split('XXX')
+    if next[-11:] != '.amazon.com':
+        next = next + '.amazon.com'
+
+    ups_vlan = input("Enter UPS vlan (Retrievable running 'sh vlan brief' in any device) :")
+
+    device_models = []
+    while(True):
+        full_model = input("Insert all access points models, when finished input 'end' (Retrievable running 'sh cdp ne' in an acc-sw): ")
+        model = full_model.split('-')[0]
+        if model == 'end': break
+        device_models.append(model)
+
+    number = 0
+    error = 0
+
+    for i in range(4):
+        thread = myThread(numberlock, errorlock)
+        thread.start()
 
     print('Dovrei aver finito!')
 
